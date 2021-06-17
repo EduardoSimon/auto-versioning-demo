3d4f9d7e0ddcc69297d96c1c3395aeff523154e1
Edu Simon <eduardo.simon@flywire.com>
2021-06-17 18:18:19 +0200

    feat: 🎸 add new foo method

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ var version = require('./package.json').version;
 const greet = () => {
 	console.log(`greetings from ${version}`);
 }
+const foo = () => {
+	console.log(`bar`);
+}
 
+exports.foo = foo;
 exports.greet = greet;
